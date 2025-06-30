253ac31123f65a09ae4e2e4c1d4918dde2693189
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-06-30 16:24:51 +0500

    profile error message fix in listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -24,6 +24,8 @@ export default function Profile() {
   const [isEditing, setIsEditing] = useState(false);
   const [showListingError, setShowListingError] = useState(false);
   const [userListings, setUserListings] = useState([]);
+  const [listingClicked, setListingClicked] = useState(false); // 👈 track Show Listings click
+
   const [formData, setFormData] = useState({
     username: currentUser.username || "",
     email: currentUser.email || "",
@@ -180,6 +182,7 @@ export default function Profile() {
 
   const handleShowListings = async () => {
     try {
+      setListingClicked(true);
       setShowListingError(false);
       const res = await fetch(`/api/user/listings/${currentUser._id}`, {
         credentials: "include", // ✅ Auth required
@@ -306,6 +309,10 @@ export default function Profile() {
         Show Listings
       </button>
       <p className="text-red-700 mt-5">{showListingError ? 'Error Showing Listings' : ''}</p>
+      {listingClicked && !showListingError && userListings.length === 0 && (
+  <p className="text-center text-gray-600 mt-5">No listings found.</p>
+)}
+
 
       {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
