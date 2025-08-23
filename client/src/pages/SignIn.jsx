6029850afcbe5dba592b6e6fc6c925351d962719
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-08-24 00:16:24 +0500

    added vercel.json

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -22,7 +22,7 @@ export default function SignIn() {
   dispatch(signInStart());
 
   try {
-    const res = await fetch('/api/auth/signin', {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/signin`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -26,7 +26,7 @@ export default function SignUp() {
   console.log(true);
   
   try{
-  const res = await fetch('/api/auth/signup', {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/signup`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
