0e3073c8cb4c2db5b3dc6f777dfb55ac4b370568
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-08-24 12:18:38 +0500

    changing frontend url in index.js

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://real-estate-frontend-opal.vercel.app'],
+  origin: ['http://localhost:5173','https://taniva.vercel.app'],
   credentials: true, // if you use cookies, otherwise can be false
 }));
 
