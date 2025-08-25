1d0acd390d0cadea76ebd45703e5cdde431274ce
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-08-25 16:03:32 +0500

    fix home responsivness

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -98,16 +98,14 @@ export default function Home() {
         </div>
 
         {/* Right Image */}
-        <div className="flex-1 relative w-full h-[60vh] sm:h-[70vh] lg:h-[80vh] rounded-2xl overflow-hidden shadow-xl border border-[#c1c4b5]">
-          <div
-            className="absolute inset-0 bg-cover bg-center scale-110 animate-zoom-slow transition-transform duration-[120s]"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-            }}
-          ></div>
-          <div className="absolute inset-0 bg-black/20"></div>
-        </div>
+         <div className="flex-1 relative w-full h-[40vh] sm:h-[60vh] lg:h-[80vh] rounded-2xl overflow-hidden shadow-xl border border-[#c1c4b5]">
+    <img
+      src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=1470&auto=format&fit=crop"
+      alt="Hero Property"
+      className="w-full h-full object-cover scale-110 animate-zoom-slow transition-transform duration-[120s]"
+    />
+    <div className="absolute inset-0 bg-black/20"></div>
+  </div>
       </div>
 
       {/* Featured Properties Swiper */}
